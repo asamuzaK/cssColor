bb611b8f5ce2384534dcf67d28684217936bdcdd
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-11-17 16:59:48 +0900

    Update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,1 @@
-export { convert } from "./js/convert.js";
-export const cachedResults: LRUCache<{}, {}, unknown>;
-export function resolve(color: string, opt?: {
-    currentColor?: string;
-    format?: string;
-    key?: any;
-}): (string | any[]) | null;
-export function parse(value: string, opt?: {
-    d50?: boolean;
-}): Array<number>;
-import { LRUCache } from 'lru-cache';
+export { convert, parse, resolve } from "./js/api.js";
---
+++ types/js/api.d.ts
@@ -0,0 +1,11 @@
+export { convert } from "./convert.js";
+export const cachedResults: LRUCache<{}, {}, unknown>;
+export function resolve(color: string, opt?: {
+    currentColor?: string;
+    format?: string;
+    key?: any;
+}): (string | any[]) | null;
+export function parse(value: string, opt?: {
+    d50?: boolean;
+}): Array<number>;
+import { LRUCache } from 'lru-cache';
