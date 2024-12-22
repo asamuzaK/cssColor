e8dbd5dc94e7ed804869456e7c298de6623becc1
asamuzaK (Kazz) <kazz@asamuzak.jp>
2024-12-22 18:19:05 +0900

    Update types

--- types/js/util.d.ts
+++ types/js/util.d.ts
@@ -1,2 +1,3 @@
 export function isColor(value: string): boolean;
 export function valueToJsonString(value: any, func?: boolean): string;
+export function interpolateHue(hueA: number, hueB: number, arc?: string): any[];
