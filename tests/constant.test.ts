72a541ceef8694df0f6549702a5a1c896b9f5238
Corbin Crutchley <git@crutchcorn.dev>
2025-01-11 11:54:41 -0800

    chore: migrate constant.test to use Vitest

--- tests/constant.test.ts
+++ tests/constant.test.ts
@@ -2,19 +2,15 @@
  * constant.test.js
  */
 
-/* api */
-import { strict as assert } from 'node:assert';
-import { describe, it } from 'mocha';
-
-/* test */
+import { describe, it, expect } from 'vitest';
 import * as constant from '../src/js/constant.js';
 
 describe('constants', () => {
   const items = Object.entries(constant);
   for (const [key, value] of items) {
     it('should get string, number or array', () => {
-      assert.strictEqual(/^[A-Z][A-Z_\d]+$/.test(key), true, 'key');
-      assert.strictEqual(typeof value, 'string', 'value');
+      expect(/^[A-Z][A-Z_\d]+$/.test(key)).toBe(true);
+      expect(typeof value).toBe('string');
     });
   }
 });
