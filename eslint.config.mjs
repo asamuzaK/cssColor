02ab0b4c336fc1b697b6037129f3534237f6fb6e
Corbin Crutchley <git@crutchcorn.dev>
2025-01-11 10:50:01 -0800

    chore: WIP major overhaul of config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,54 +1,13 @@
-import importX from 'eslint-plugin-import-x';
-import jsdoc from 'eslint-plugin-jsdoc';
-import regexp from 'eslint-plugin-regexp';
-import globals from 'globals';
-import neostandard, { plugins as neostdplugins } from 'neostandard';
+// @ts-check
+
+// @ts-ignore Needed due to moduleResolution Node vs Bundler
+import { tanstackConfig } from '@tanstack/config/eslint'
 
 export default [
-  ...neostandard({
-    semi: true
-  }),
-  jsdoc.configs['flat/recommended'],
-  regexp.configs['flat/recommended'],
+  ...tanstackConfig,
   {
-    ignores: ['dist/']
-  },
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.webextensions
-      }
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: true
-    },
-    plugins: {
-      '@stylistic': neostdplugins['@stylistic'],
-      'import-x': importX,
-      regexp
-    },
+    name: 'tanstack/temp',
     rules: {
-      '@stylistic/space-before-function-paren': ['error', {
-        anonymous: 'always',
-        asyncArrow: 'always',
-        named: 'never'
-      }],
-      'import-x/order': ['error', {
-        alphabetize: {
-          order: 'ignore',
-          caseInsensitive: false
-        }
-      }],
-      'no-await-in-loop': 'error',
-      'no-loss-of-precision': 'off',
-      'no-use-before-define': ['error', {
-        allowNamedExports: false,
-        classes: true,
-        functions: false,
-        variables: true
-      }]
-    }
-  }
-];
+    },
+  },
+]
