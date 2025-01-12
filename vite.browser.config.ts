18aa75daf382c08fd2f1c1b7da9a0f3c211daa96
Corbin Crutchley <git@crutchcorn.dev>
2025-01-12 06:17:39 -0800

    chore: add minified browser supported file

---
+++ vite.browser.config.ts
@@ -0,0 +1,32 @@
+import esbuild from 'esbuild';
+import { defineConfig, Plugin } from 'vite'
+
+const minifyBundle = ():Plugin=>({
+  name:"minify-bundle",
+  async generateBundle(_,bundle)
+  {
+    for (const asset of Object.values(bundle))
+    {
+      if (asset.type == "chunk")
+        asset.code = (await esbuild.transform(asset.code,{minify:true, legalComments: "none"})).code;
+    }
+  }
+});
+
+export default defineConfig({
+  plugins: [minifyBundle()],
+  ssr: {
+    noExternal: true
+  },
+  build: {
+    emptyOutDir: false,
+    outDir: "dist",
+    lib: {
+      entry: "src/index.ts",
+      formats: ['es'],
+      fileName: () => {
+        return '[name].min.js'
+      },
+    },
+  }
+})
