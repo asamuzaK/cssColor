dffc67e01f1ff724f0b9e10e468240d5ae6c6dc3
asamuzaK (Kazz) <kazz@asamuzak.jp>
2025-02-18 18:02:26 +0900

    Update typedef.ts

--- src/js/typedef.ts
+++ src/js/typedef.ts
@@ -42,7 +42,7 @@ export type StringColorChannels = [
 ];
 
 /**
- * @type StringColorSpaceedChannels - specified value
+ * @type StringColorSpacedChannels - specified value
  */
 export type StringColorSpacedChannels = [
   cs: string,
