9bab136b86ce332cd9596d4a969cf4dab34936d9
asamuzaK (Kazz) <asamuzaK@users.noreply.github.com>
2026-06-21 16:34:34 +0900

    Update default cache size

--- src/js/cache.ts
+++ src/js/cache.ts
@@ -6,7 +6,7 @@ import { GenerationalCache } from '@asamuzakjp/generational-cache';
 import { Options } from './typedef';
 
 /* constants */
-const CACHE_SIZE = 1024;
+const CACHE_SIZE = 2048;
 
 /**
  * CacheItem
